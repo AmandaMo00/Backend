080178b2a99ddf3ed805948eb6f2e9d966f6140c
AmandaMo00 <huanghuiyi002@chinasofti.com>
2024-10-09 17:43:10 +0800

    add LDAP authentication and csv export

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.forms import ModelForm
 from django.http import HttpRequest
+from interview.admin import export_model_as_csv
 from jobs.models import Job
 
 # Register your models here.
@@ -8,6 +9,15 @@ class JobAdmin(admin.ModelAdmin):
     #定义展示字段和隐藏字段
     list_display = ('job_name','job_type','job_city','creator','create_date','modify_date')
     exclude = ('creator','create_date','modify_date')
+
+    # 增加actions
+    actions = [export_model_as_csv,]
+
+    # 当前用户是否有导出权限：
+    def has_export_permission(self, request):
+        opts = self.opts
+        return request.user.has_perm('%s.%s' % (opts.app_label, "export"))
+
 
     def save_model(self, request, obj, form, change) :
         obj.creator = request.user
